b766b4fca1ec57b79fd1311e18f17668f2a2a53f
Dmytro-Plot <capital2010@ukr.net>
2024-03-07 19:08:34 +0200

    modull6

--- модуль6/home1.py
+++ модуль6/home1.py
@@ -55,35 +55,27 @@ class AddressBook(UserDict):
             del self.data[name]
 
 
-# Створення нової адресної книги
-book = AddressBook()
+book = AddressBook() # Створення нової адресної книги
 
-# Створення запису для John
-john_record = Record("John")
+john_record = Record("John") # Створення запису для John
 john_record.add_phone("1234567890")
 john_record.add_phone("5555555555")
 
-# Додавання запису John до адресної книги
-book.add_record(john_record)
+book.add_record(john_record) # Додавання запису John до адресної книги
 
-# Створення та додавання нового запису для Jane
-jane_record = Record("Jane")
+jane_record = Record("Jane") # Створення та додавання нового запису для Jane
 jane_record.add_phone("9876543210")
 book.add_record(jane_record)
 
-# Виведення всіх записів у книзі
-for name, record in book.data.items():
+for name, record in book.data.items(): # Виведення всіх записів у книзі
     print(record)
 
-# Знаходження та редагування телефону для John
-john = book.find("John")
+john = book.find("John") # Знаходження та редагування телефону для John
 john.edit_phone("1234567890", "1112223333")
 
 print(john)  # Виведення: Contact name: John, phones: 1112223333; 5555555555
 
-# Пошук конкретного телефону у записі John
-found_phone = john.find_phone("5555555555")
+found_phone = john.find_phone("5555555555") # Пошук конкретного телефону у записі John
 print(f"{john.name.value}: {found_phone}")  # Виведення: 5555555555
 
-# Видалення запису Jane
-book.delete("Jane")
+book.delete("Jane") # Видалення запису Jane
